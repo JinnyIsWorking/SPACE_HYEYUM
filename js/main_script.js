6b74e9311a4718cac8161f682978840e159711ae
JinnyIsWorking <145026587+JinnyIsWorking@users.noreply.github.com>
2023-09-15 17:54:55 +0900

    Add files via upload

--- js/main_script.js
+++ js/main_script.js
@@ -41,13 +41,13 @@ $(window).scroll(function(){
 });
 
 
-// current bg ????????????????
-$('.title_bg').hide();
-// $('#banner ul li:nth-child(1)').show();
+// current bg
+$('.title_bg ul li').hide();
+$('.title_bg ul li:nth-child(1)').show();
 
 setInterval(function(){
-    $('.title_bg ul li').fadeIn(1700).next().fadeIn(2500).end().appendTo('.title_bg ul');
+    $('.title_bg ul li:nth-child(1)').hide().next().show().end().appendTo('.title_bg ul');
     // .end() 메서드는 재선택된 요소의 이전 요소를 선택하는 메서드입니다.
     // appendTo()는 선택한 요소를 다른 요소 안에 넣습니다.
     // .end().appendTo() -> .end() 선택된 요소를 .appendTo()의 소괄호 안에 넣는다.
-},300);
+},500);
